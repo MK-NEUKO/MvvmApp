ea6a233e1ba29df9f932bc36e6d6b85f4c3ff29c
EngineerSpock <engineerspock@outlook.com>
2016-05-20 09:21:23 +0300

    Add project files.

---
+++ StudentsManager/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using DataAccess;
+using Models;
+
+namespace StudentsManager {
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private StudentsXmlProvider studentsXmlProvider;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private bool ListHasSelectedItems {
+            get { return StudentsList.SelectedItems != null && StudentsList.SelectedItems.Count > 0; }
+        }
+
+        protected override void OnInitialized(EventArgs e) {
+            base.OnInitialized(e);
+
+            ReloadStudents();
+        }
+
+        private void ExitFromApp_OnClick(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void AddStudent_OnClick(object sender, RoutedEventArgs e) {
+            var addStudent = new AddEditStudentWindow(studentsXmlProvider);
+            addStudent.ShowDialog();
+
+            ReloadStudents();
+        }
+
+        private void ReloadStudents() {
+            StudentsList.Items.Clear();
+
+            studentsXmlProvider = new StudentsXmlProvider("StudentsRepo.xml");
+            var students = studentsXmlProvider.GetAll();
+
+            foreach (var student in students) {
+                StudentsList.Items.Add(student);
+            }
+        }
+
+        private void EditStudent_OnClick(object sender, RoutedEventArgs e) {
+            if (ListHasSelectedItems) {
+                var student = StudentsList.SelectedItems[0] as Student;
+                var addStudent = new AddEditStudentWindow(student, studentsXmlProvider);
+                addStudent.ShowDialog();
+
+                ReloadStudents();
+            }
+        }
+
+        private void RemoveStudent_OnClick(object sender, RoutedEventArgs e) {
+            if (ListHasSelectedItems) {
+                var item = StudentsList.SelectedItems?[0] as Student;
+                studentsXmlProvider.Remove(item.Id);
+                RemoveFromListBox(item);
+            }
+        }
+
+        private void RemoveFromListBox(Student item) {
+            StudentsList.Items.Remove(item);
+        }
+
+        private void SubmitChanges_OnClick(object sender, RoutedEventArgs e) {
+            try {
+                studentsXmlProvider.SubmitChanges();
+            }
+            catch (Exception ex) {                
+            }
+        }
+    }
+}
