627eed71686067e15871622d64b9c5c7ccf70c17
Michael Koch <m.koch@know-how-akademie.de>
2021-02-20 22:47:37 +0100

    Initial Commit

---
+++ StudentsManager/MainView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using DataAccess;
+using StudentsManager.ViewModels;
+
+//using DataAccess;
+//using Models;
+
+namespace StudentsManager {
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window {
+        public MainView() {
+            InitializeComponent();
+            DataContext = new MainViewModel(new StudentsXmlProvider("StudentsRepo.xml"));
+        }
+
+        private void ExitFromApp_OnClick(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void AddStudent_OnClick(object sender, RoutedEventArgs e) {
+        }
+
+        private void EditStudent_OnClick(object sender, RoutedEventArgs e) {
+        }
+
+        private void RemoveStudent_OnClick(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void SubmitChanges_OnClick(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using DataAccess;
+using Models;
+
+namespace StudentsManager.ViewModels 
+{
+    public class MainViewModel 
+    {
+        private IDataProvider<Student> studentsXmlProvider;
+        public ObservableCollection<Student> Students { get; private set; }
+
+        public MainViewModel(IDataProvider<Student> provider) 
+        {
+            studentsXmlProvider = provider;
+            ReloadStudents();
+        }
+
+        private void ReloadStudents() 
+        {            
+            var students = studentsXmlProvider.GetAll();
+            Students = new ObservableCollection<Student>(students);
+        }
+    }
+}
